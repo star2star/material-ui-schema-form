0cc1f7783a5b7a510607d5cf22d1a502a852128e
Jean M. Lescure <jlescure@18techs.com>
2017-12-04 06:41:02 -0600

    Enhanced TextArea multiline capability

--- src/TextArea.js
+++ src/TextArea.js
@@ -9,17 +9,17 @@ class FormTextArea extends Component {
     return (
       <div className={this.props.form.htmlClass}>
         <TextField
-          multiLine
+          multiline
           type={this.props.form.type}
           label={this.props.form.title}
           placeholder={this.props.form.placeholder}
           helperText={this.props.error}
-          error={this.props.error}
+          error={(this.props.error)? true : false}
           onChange={this.props.onChangeValidate}
           defaultValue={this.props.value}
           disabled={this.props.form.readonly}
           style={this.props.form.style || {width: '100%'}}
-          rows={this.props.form.rows}
+          rows={this.props.form.rows || 4}
           rowsMax={this.props.form.rowsMax}
         />
       </div>
